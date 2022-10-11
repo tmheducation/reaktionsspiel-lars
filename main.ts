1dc15caef2974b0d59285c4b661ee282d2210555
tmheducation <68049755+tmheducation@users.noreply.github.com>
2022-10-11 12:31:49 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-	
+    music.playMelody("C D E F G - G F ", 12111)
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
 	
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-	
+    music.playMelody("C5 B A G F E D C ", 1209)
 })
 basic.forever(function () {
-    music.playMelody("C5 B A G F E D C ", 1209)
+	
 })
