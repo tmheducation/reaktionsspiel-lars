5314aa90b1b7e9d612dde51b7332cf858361819e
tmheducation <68049755+tmheducation@users.noreply.github.com>
2022-10-11 12:10:32 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+	
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+	
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+	
+})
+basic.forever(function () {
+    music.playMelody("C5 B A G F E D C ", 1209)
+})
